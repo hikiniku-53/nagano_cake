abb9332d0ce6fcdf70d36ade76cca60c05903bdd
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-04-25 12:51:20 +0000

    CONTROLLERS_FILE_CREATED

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "282f23aed40647ee88b300fc1e4e7952.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    resistrations: "public/resistrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/home/about' => 'homes#about', as: 'about'
+
 end
