8174ccc63a1da2b7dcfc2c0defc325516ec9318f
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-05-01 00:50:25 +0000

    0430_FINISH

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.create!(
+  email: "admin@gmail.com",
+  password: "nagano_cake",
+  admin: true
+  )
