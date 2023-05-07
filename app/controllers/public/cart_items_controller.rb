07b88e34d5b7ca26175b0e56c60b6a04ba2dfa0c
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-05-07 08:38:01 +0000

    0507_1

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,28 @@
 class Public::CartItemsController < ApplicationController
-    def index
-    end
+  def index
+    @cart_items = CartItem.all
+  end
 
-    def create
-    end
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-    end
+  def update
+  end
 
-    def destroy
-    end
+  def destroy
+  end
 
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,19 @@
 class Public::ItemsController < ApplicationController
 
-    def index
-    end
+  def index
+    @items = Item.all
+  end
 
-    def show
-    end
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
 
-    def genre_search
-    end
+  def genre_search
+  end
 
-    def search
-    end
+  def search
+  end
 
 
 end
